bdbe70eb6da2a887ce844c15f11fdd9ed36a794f
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-05 15:21:17 +0900

    초기 커밋.

---
+++ Text_RPG/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Text_RPG_Console
+{
+    internal class Text_RPG
+    {
+        // 플레이어 클래스
+        class Player
+        {
+            public string name { get; set; } = "";
+            public int lv { get; set; } = 1;
+            public string job { get; set; } = "( 전사 )";
+            public int att { get; set; } = 10;
+            public int df { get; set; } = 5;
+            public int hp { get; set; } = 100;
+            public int gold { get; set; } = 1500;  // 초기 골드
+        }
+
+    }
+}
